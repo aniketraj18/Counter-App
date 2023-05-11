208dc3cde0a56a604a02662343cc4eb9851c0d4b
Aniket Raj <84118141+aniketraj18@users.noreply.github.com>
2023-05-12 00:19:24 +0530

    Add files via upload

---
+++ counter app/main.js
@@ -0,0 +1,26 @@
+//buttons and counter text
+const counter=document.querySelector('#counter')
+const btns=document.querySelectorAll('.btn')
+let count=0
+btns.forEach((btn) =>{
+    btn.addEventListener('click',(e)=>{
+        const styles=e.currentTarget.classList
+        if(styles.contains('increase')){
+            count++;
+        }
+        else if(styles.contains('decrease')){
+            count--;
+        }
+        else
+        count=0;
+        if(count>0){
+            counter.style.color='green';
+        }
+        else if(count===0)
+        counter.style.color='grey';
+        else
+        counter.style.color='red';
+        counter.textContent=count;
+    })
+
+})
